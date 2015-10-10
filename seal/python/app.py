4422f747efb4448b78f213e4db2022e53f90ddde
Joshua Hartley <jhartley@jhartley.net>
2015-10-10 00:51:14 -0400

    Finished with basic api methods

--- seal/python/app.py
+++ seal/python/app.py
@@ -150,7 +150,105 @@ def get_scheduled_trips():
 
 @app.route(URL_PREFIX + '/reservation', methods=['POST'])
 def create_reservation():
-    pass
+    if not request.json:
+        abort(400)
+
+    r = request.json
+    user = model.User.query.filter_by(id=r["user_id"]).first()
+    scheduled_trip = model.ScheduledTrip.query.filter_by(id=r["sch_trip_id"]).first()
+
+    if user is None:
+        abort(400)
+    if scheduled_trip is None:
+        abort(400)
+
+    reservation = model.TripReservation(user, scheduled_trip)
+    user.trip_reservations.append(reservation)
+    db.session.commit()
+
+
+    response = {
+            'id': reservation.id
+            }
+    return flask.jsonify(**response)
+
+@app.route(URL_PREFIX + '/reservation/<int:userId>', methods=['GET'])
+def get_reservations_for_user(userId):
+    user = model.User.query.filter_by(id=userId).first()
+
+    if user is None:
+        results = []
+    else:
+        results = [item.to_json() for item in user.trip_reservations]
+        results = []
+        for item in user.trip_reservations:
+            found = False
+            if len(item.scheduledtrip.trips) == 0:
+                results.append(item.to_json())
+                continue
+            for passenger in item.scheduledtrip.trips[0].trip_passengers:
+                if passenger.user_id == user.id:
+                    found = True
+                    break
+            if not found:
+                results.append(item.to_json())
+
+    response = {
+            'results': results
+            }
+    return flask.jsonify(**response)
+
+
+@app.route(URL_PREFIX + '/confirm/<int:reservationId>', methods=['POST'])
+def confirm_reservation(reservationId):
+    if not request.json:
+        abort(400)
+
+    r = request.json
+    carId = r["car_id"]
+
+
+    car = model.Car.query.filter_by(id=carId).first()
+    reservation = model.TripReservation.query.filter_by(id=reservationId).first()
+    sch_trip = reservation.scheduledtrip
+
+    user = reservation.user
+    if len(sch_trip.trips) <= 0:
+        ntrip = model.Trip(sch_trip, car)
+        sch_trip.trips.append(ntrip)
+    else:
+        ntrip = sch_trip.trips[0]
+
+    emis_saved = 0
+    fuel_saved = 0
+    npassenger = model.TripPassenger(ntrip, user, emis_saved, fuel_saved)
+
+    ntrip.trip_passengers.append(npassenger)
+    db.session.commit()
+
+
+    response = {
+            'emission_saved': emis_saved,
+            'fuel_saved': fuel_saved
+            }
+    return flask.jsonify(**response)
+
+@app.route(URL_PREFIX + '/stats/<int:userId>', methods=['GET'])
+def stats(userId):
+    user = model.User.query.filter_by(id=userId).first()
+    emis_saved = 0
+    fuel_saved = 0
+    for trip in user.trip_passengers:
+        emis_saved += trip.emissions_saved
+        fuel_saved += trip.fuel_saved
+
+
+    response = {
+            'emission_saved': emis_saved,
+            'fuel_saved': fuel_saved
+            }
+    return flask.jsonify(**response)
+
 
 
 if __name__ == '__main__':
